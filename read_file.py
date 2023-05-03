0e17d5bce4a899fb18231331e69c996de0b937a5
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-05-03 11:27:29 +0200

    Added function that reads the whole file into a string

--- read_file.py
+++ read_file.py
@@ -1,7 +1,7 @@
-# Function that returns a given amount of random words from input file
 import random
 
 
+# Function that returns a given length list of random words from file
 def read_words_from_file(path, n):
     words_list = []
     final_list = []
@@ -12,3 +12,14 @@ def read_words_from_file(path, n):
         final_list = random.choices(words_list, k=n)
         file_handle.close()
     return final_list
+
+
+# Function that reads all the words from a file into a string
+def read_whole_file(path):
+    final_string = ""
+    with open(path, "r") as file_handle:
+        for line in file_handle:
+            for word in line.split():
+                final_string += f"{word} "
+        file_handle.close()
+    return final_string
