4d4b535cc8e359407094d87dd7330e96423b0a2b
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-05-03 11:54:35 +0200

    Improved units

--- test_pattern.py
+++ test_pattern.py
@@ -2,11 +2,11 @@ import random
 
 
 # Function that returns a random test text created with letters 'A' and 'B'
-# It's length is always between 20 and a 1000
+# It's length is always between 50 and a 1000
 def random_text():
     text = ""
     letters = ["A", "B"]
-    text_length = random.randint(20, 1000)
+    text_length = random.randint(50, 1000)
     for i in range(text_length):
         text += random.choice(letters)
     return text
