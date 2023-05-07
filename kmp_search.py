97ca02e3761e8831f0525c55abaa6607726b6b6e
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-05-07 22:31:19 +0200

    Initial KMP layout

---
+++ kmp_search.py
@@ -0,0 +1,20 @@
+def kmp_search(pattern, text):
+    """
+    Params:
+    pattern (str) - searched text pattern
+    text (str) - string to search for patterns in
+    Returns:
+    occurences (list) - list of pattern starting indexes in text
+    """
+    occurences = []
+    lp = len(pattern)
+    lt = len(text)
+    if lp > lt:
+        return None
+    elif lp == lt:
+        if pattern == text:
+            return [0]
+        else:
+            return None
+    else:
+        pass
