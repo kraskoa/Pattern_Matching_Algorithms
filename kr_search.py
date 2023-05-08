294f9682ab94eaa545508452f521cbcbd19eae80
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-05-08 21:55:42 +0200

    Basic KR implementation guidelines

---
+++ kr_search.py
@@ -0,0 +1,23 @@
+# Dla ciebie Krzysiu zostal ten algorytm Karpa-Rabina i klasyczne sklejenie reszty maina
+# Wstaw na poczatku definicji swojej funkcji cos na zasadzie:
+#     lp = len(pattern)
+#     lt = len(text)
+#     if lp > lt:
+#         return None
+#     elif lp == lt:
+#         if pattern == text:
+#             return [0]
+#         else:
+#             return None
+#     else:
+#         {definicja funkcji}
+# Bedzie sensowna spojnosc implementacji wtedy
+def kmp_search(pattern, text):
+    """
+    Params:
+    pattern (str) - searched text pattern
+    text (str) - string to search for patterns in
+    Returns:
+    occurences (list) - list of pattern starting indexes in text
+    """
+    pass
